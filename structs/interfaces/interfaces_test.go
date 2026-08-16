2bb6171c2f5d426f8e7701a689bffeed42d52eab
Naresh Pentala <itsnareshpentala@outlook.com>
2026-08-16 15:56:44 +0530

    advanced tdt

--- structs/interfaces/interfaces_test.go
+++ structs/interfaces/interfaces_test.go
@@ -37,15 +37,43 @@ func TestAre(t *testing.T){
 		shape Shape
 		want float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 31.41592653589793},
-		{Triangle{12, 6}, 36.0},
+		{shape: Rectangle{Width: 12, Length: 6}, want: 72.0},
+        {shape: Circle{Radius: 10}, want: 314.1592653589793},
+        {shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		// {Rectangle{12, 6}, 72.0},
+		// {Circle{10}, 31.41592653589793},
+		// {Triangle{12, 6}, 31.0},
 	}
 
 	for _, tt := range areaTests {
 		got := tt.shape.Area()
 		if got != tt.want {
-			t.Errorf("got %g but want %g ", got, tt.want)
+			t.Errorf(" %#v got %g want %g ", got, tt.want)
 		}
 	}
+}
+
+func TestAdvancedArea(t *testing.T) {
+
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Length: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+
+	}
+
 }
