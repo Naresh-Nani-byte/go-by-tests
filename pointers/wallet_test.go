fdc17a4f273ce754db362fd5b5fc01c1391d338f
Naresh Pentala <itsnareshpentala@outlook.com>
2026-08-20 18:07:03 +0530

    refactoring the code

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -4,16 +4,20 @@ import "testing"
 
 func TestWallet(t *testing.T){
 
+	assertBalance := func (t testing.TB, wallet Wallet, want Bitcoin)  {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %d but want %d", got, want)
+		}
+	}
+
 	t.Run("Deposit the Bitcoin ", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(20))
 
-		got := wallet.Balance()
-		want := Bitcoin(20)
-
-		if got != want {
-			t.Errorf("got %d but want %d ", got, want)
-		}
+		assertBalance(t, wallet, 20)
 	})	
 
 	t.Run("Bitcoing String ", func(t *testing.T) {
@@ -29,10 +33,6 @@ func TestWallet(t *testing.T){
 	t.Run("Wothdraw the bitcoing", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
-		got := wallet.Balance()
-		want := Bitcoin(10)
-		if got != want {
-			t.Errorf("got %d but want %d ", got, want)
-		}
+		assertBalance(t, wallet, 10)
 	})
 }
