7ad884c74f2e32ed154f3ca16701287108cccc31
Naresh Pentala <itsnareshpentala@outlook.com>
2026-08-20 18:02:33 +0530

    sample pointers on basic banking functionalities

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,14 +1,29 @@
 package pointers
 
+import "fmt"
+
+type Bitcoin int
 
 type Wallet struct{
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+type Stringers interface{
+	String() string
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int{
+func (w *Wallet) Balance() Bitcoin{
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin){
+	w.balance -= amount
+}
+
+func (b Bitcoin) String() string{
+	return  fmt.Sprintf("%d BTC", b)
 }
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -6,13 +6,33 @@ func TestWallet(t *testing.T){
 
 	t.Run("Deposit the Bitcoin ", func(t *testing.T) {
 		wallet := Wallet{}
-		wallet.Deposit(10)
+		wallet.Deposit(Bitcoin(20))
 
 		got := wallet.Balance()
-		want := 10
+		want := Bitcoin(20)
 
 		if got != want {
 			t.Errorf("got %d but want %d ", got, want)
 		}
 	})	
+
+	t.Run("Bitcoing String ", func(t *testing.T) {
+		btc := Bitcoin(10)
+		got := btc.String()
+		want := "10 BTC"
+
+		if got != want {
+			t.Errorf("got %s but want %s",got, want)
+		}
+	})
+
+	t.Run("Wothdraw the bitcoing", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(10)
+		if got != want {
+			t.Errorf("got %d but want %d ", got, want)
+		}
+	})
 }
